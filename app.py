0f634b81a6d301354e8b7b30a454b4810c3b2107
Anna Rechtackova <xrechtac@fi.muni.cz>
2022-08-20 16:27:37 +0200

    edulint: use right function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, flash, send_from_directory, send_file
 from edulint.config.config import get_config
-from edulint.linting.linting import lint
+from edulint.linting.linting import lint_one
 from edulint.linting.problem import Problem
 from edulint.explanations import get_explanations
 import os
@@ -74,7 +74,7 @@ def analyze(code_hash: str):
             return f.read()
 
     config = get_config(code_path(code_hash))
-    result = lint(code_path(code_hash), config)
+    result = lint_one(code_path(code_hash), config)
 
     result_json = Problem.schema().dumps(result, indent=2, many=True)
     with open(problems_path(code_hash), "w", encoding="utf8") as f:
