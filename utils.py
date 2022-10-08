8ca94290d82f17e941155b0ea2061aaf0c093f48
Anna Rechtackova <xrechtac@fi.muni.cz>
2022-10-08 15:38:41 +0200

    back: api uses version

--- utils.py
+++ utils.py
@@ -1,13 +1,43 @@
 import os
+from typing import Tuple, Optional
+from dataclasses import dataclass
 
 
-def full_path(upload_folder: str, filename: str) -> str:
-    return os.path.join(upload_folder, filename)
+@dataclass(init=False, order=True)
+class Version:
+
+    version: Tuple[int, int, int]
+
+    def __init__(self, version_raw):
+        self.version = Version.parse(version_raw)
+
+    def name(self) -> str:
+        return "_".join(self.version)
+
+    def version_str(self) -> str:
+        return ".".join(self.version)
+
+    def dir(self, prefix: str) -> str:
+        return f"{prefix}_{self.name()}"
+
+    @staticmethod
+    def parse(version_raw: str) -> Optional["Version"]:
+        version_split = version_raw.split(".")
+
+        if len(version_split) != 3 or not all(v.is_decimal() for v in version_split):
+            return None
+
+        return tuple([int(v) for v in version_split])
+
+
+def full_path(upload_folder: str, filename: str, version: Optional[Version] = None) -> str:
+    version_name = f"_{version.name()}" if version is not None else ""
+    return os.path.join(upload_folder, f"{filename}{version_name}")
 
 
 def code_path(upload_folder: str, code_hash: str) -> str:
     return full_path(upload_folder, code_hash) + ".py"
 
 
-def problems_path(upload_folder: str, code_hash: str) -> str:
-    return full_path(upload_folder, code_hash) + ".json"
+def problems_path(upload_folder: str, code_hash: str, version: Version) -> str:
+    return full_path(upload_folder, code_hash, version) + ".json"
