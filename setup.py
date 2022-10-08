e81e49d290153fe899f0d756777c6aff668d0d9d
Anna Rechtackova <xrechtac@fi.muni.cz>
2022-10-08 18:26:58 +0200

    deploy: install available edulint versions in setup

---
+++ setup.py
@@ -0,0 +1,45 @@
+import requests
+import toml
+import subprocess
+import sys
+import os
+
+from utils import Version
+
+
+CONFIG = {
+    "CODE_FOLDER": "codes",
+    "ANALYSIS_FOLDER": "analyses",
+    "VERSIONS_FOLDER": "versions",
+    "LINTER_FOLDER_PREFIX": "edulint",
+    "EXPLANATIONS": "explanations.json",
+}
+
+
+def get_versions():
+    def filter(versions):
+        return [v for v in versions if v >= Version("0.0.12")]
+
+    edulint_info = requests.get("https://pypi.org/pypi/edulint/json").json()
+    return filter([Version(v) for v in edulint_info["releases"].keys()])
+
+
+def prepare_config(config, versions):
+    config = config.copy()
+    config["VERSIONS"] = [str(v) for v in versions]
+
+    with open("config.toml", "w") as f:
+        f.write(toml.dumps(config))
+
+
+def prepare_packages(config, versions):
+    for version in versions:
+        version_folder = os.path.join(config['VERSIONS_FOLDER'], f"{config['LINTER_FOLDER_PREFIX']}_{version.name()}")
+        subprocess.check_call(f"{sys.executable} -m pip install edulint=={version} --target={version_folder}".split())
+
+
+if __name__ == "__main__":
+    versions = get_versions()
+
+    prepare_packages(CONFIG, versions)
+    prepare_config(CONFIG, versions)
